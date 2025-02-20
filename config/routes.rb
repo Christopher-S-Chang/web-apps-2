e5fab19a38221ae993c7e81fd8c60e5e25be1a19
Christopher Chang <christopher.chang1@kellogg.northwestern.edu>
2025-02-20 02:31:17 +0000

    Companies creating

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources "companies"
   # get("/companies", {:controller => "companies", :action => "index"})
+  
   resources "contacts"
   # get("/contacts", {:controller => "contacts", :action => "index"})
 
