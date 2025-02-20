5c3cd6d5d887192bff1c2a9cb6226c7bc5741330
Christopher Chang <christopher.chang1@kellogg.northwestern.edu>
2025-02-20 01:34:01 +0000

    stateless html

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -1,6 +1,18 @@
 class BitcoinsController < ApplicationController
 
   def index
+    
+    @bitcoin = params["amount"]
+    require "net/http"
+    require "json"
+    url = "https://blockchain.info/ticker"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    @bitcoin_data = JSON.parse(response)
+    @usd_rate = @bitcoin_data["USD"]["last"]
+
+    # calculate value in USD of user's bitcoin
+    @usd_value = @usd_rate * @bitcoin.to_f
     # add backend code
     # render bitcoins/index view
   end
